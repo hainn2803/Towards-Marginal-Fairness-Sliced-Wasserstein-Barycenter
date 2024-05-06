6112e1b44d880094f7aee4e0ba1a0737a7ca096d
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-05-06 21:46:58 +0700

    first commit

---
+++ train_run.bash
@@ -0,0 +1,67 @@
+weight_fsw_values=(0.5)
+methods=(EFBSW FBSW lowerboundFBSW OBSW BSW)
+obsw_weights=(0.1 1.0 10.0)
+num_epochs=300
+seed_id=42
+
+python3 train.py \
+  --dataset mnist \
+  --num-classes 10 \
+  --datadir data \
+  --outdir result \
+  --distribution circle \
+  --epochs "#num_epochs" \
+  --optimizer rmsprop \
+  --lr 0.001 \
+  --alpha 0.9 \
+  --batch-size 1000 \
+  --batch-size-test 128 \
+  --seed "@seed_id" \
+  --weight_swd 8.0 \
+  --weight_fsw 0.0 \
+  --method None \
+  --saved-model-interval 100 \
+
+for weight_fsw in "${weight_fsw_values[@]}"; do
+
+    for method in "${methods[@]}"; do
+        python3 train.py \
+            --dataset mnist \
+            --num-classes 10 \
+            --datadir data \
+            --outdir result \
+            --distribution circle \
+            --epochs "@num_epochs" \
+            --optimizer rmsprop \
+            --lr 0.001 \
+            --alpha 0.9 \
+            --batch-size 1000 \
+            --batch-size-test 128 \
+            --seed "@seed_id" \
+            --weight_swd 8.0 \
+            --weight_fsw "$weight_fsw" \
+            --method "$method" \
+            --saved-model-interval 100
+    done
+
+    for lmbd in "${obsw_weights[@]}"; do
+        python3 train.py \
+            --dataset mnist \
+            --num-classes 10 \
+            --datadir data \
+            --outdir result \
+            --distribution circle \
+            --epochs "@num_epochs" \
+            --optimizer rmsprop \
+            --lr 0.001 \
+            --alpha 0.9 \
+            --batch-size 1000 \
+            --batch-size-test 128 \
+            --seed "@seed_id" \
+            --weight_swd 8.0 \
+            --weight_fsw "$weight_fsw" \
+            --method OBSW \
+            --lambda-obsw "$lmbd" \
+            --saved-model-interval 100
+    done
+done
